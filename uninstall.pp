f51e63692b7e4fe42dcaaa34ed9507a2912718a1
DevOpsTestAdmin <cmanikandan@yahoo.com>
2015-03-10 15:55:42 +0530

    Update uninstall.pp

--- uninstall.pp
+++ uninstall.pp
@@ -11,31 +11,49 @@ service { "apache2":
 
 package { "mysql-server":
 ensure => "purged",
-before => service['mysql'],
+require => service['mysql'],
 }
 
 package { "mysql-client":
 ensure => "purged",
-before => service['mysql'],
+require => service['mysql'],
 }
 
 
 package { "mysql":
 ensure => "purged",
-before => service['mysql'],
+require => service['mysql'],
 }
 
 
 package { "apache2":
 ensure => "purged",
-before => service['apache2'],
+require => service['apache2'],
 }
 
 
-exec { 'autoremove':
+exec { 'autoremove-apache':
     command => '/usr/bin/apt-get autoremove --purge -y',
     refreshonly => true,
     subscribe => package['apache2'],
+}
+
+exec { 'autoremove-mysql-server':
+    command => '/usr/bin/apt-get autoremove --purge -y',
+    refreshonly => true,
+    subscribe => package['mysql-server'],
+}
+
+exec { 'autoremove-mysql-client':
+    command => '/usr/bin/apt-get autoremove --purge -y',
+    refreshonly => true,
+    subscribe => package['mysql-client'],
+}
+
+exec { 'autoremove-mysql':
+    command => '/usr/bin/apt-get autoremove --purge -y',
+    refreshonly => true,
+    subscribe => package['mysql'],
 }
 
 
