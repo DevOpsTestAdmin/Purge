da2ffb0590534f28d7bc1ae4dd5eb70bc218760c
DevOpsTestAdmin <cmanikandan@yahoo.com>
2015-03-10 16:49:41 +0530

    Update uninstall.pp

--- uninstall.pp
+++ uninstall.pp
@@ -1,7 +1,4 @@
 
-service { "mysql":
-  ensure => "stopped",
-}
 
 
 service { "apache2":
@@ -23,48 +20,5 @@ exec { 'autoremove-apache2.2':
 }
 
 
-package { "mysql-server":
-ensure => "purged",
-require => service['mysql'],
-}
 
-package { "mysql-client":
-ensure => "purged",
-require => service['mysql'],
-}
-
-
-package { "mysql":
-ensure => "purged",
-require => service['mysql'],
-}
-
-
-exec { 'autoremove-mysql-server':
-    command => '/usr/bin/apt-get autoremove --purge -y',
-    refreshonly => true,
-    subscribe => package['mysql-server'],
-}
-
-exec { 'autoremove-mysql-client':
-    command => '/usr/bin/apt-get autoremove --purge -y',
-    refreshonly => true,
-    subscribe => package['mysql-client'],
-}
-
-exec { 'autoremove-mysql':
-    command => '/usr/bin/apt-get autoremove --purge -y',
-    refreshonly => true,
-    subscribe => package['mysql'],
-}
-
-
-
-file { "remove_wordpress":
-ensure => absent,
-path => "/var/www",
-recurse => true,
-purge => true,
-force =>true,
-}
 
