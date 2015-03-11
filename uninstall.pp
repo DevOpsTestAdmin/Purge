eafcba08e4108946d9cf6917d74b74ce39e190fd
DevOpsTestAdmin <cmanikandan@yahoo.com>
2015-03-11 12:27:02 +0530

    Update uninstall.pp

--- uninstall.pp
+++ uninstall.pp
@@ -14,6 +14,26 @@ ensure => "purged",
 require => service['apache2'],
 }
 
+package { "apache2-utils":
+ensure => "purged",
+require => package['apache2'],
+}
+
+package { "apache2.2-bin":
+ensure => "purged",
+require => package['apache2'],
+}
+
+package { "apache2.2-common":
+ensure => "purged",
+require => package['apache2'],
+}
+
+package { "apache2-mpm-prefork":
+ensure => "purged",
+require => package['apache2'],
+}
+
 
 exec { 'autoremove-apache':
     command => '/usr/bin/apt-get autoremove --purge -y',
