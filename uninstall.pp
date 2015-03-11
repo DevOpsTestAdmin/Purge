4c2d38c8db02adff94faf6606f474a57abd7e8a4
DevOpsTestAdmin <cmanikandan@yahoo.com>
2015-03-11 12:19:23 +0530

    Update uninstall.pp

--- uninstall.pp
+++ uninstall.pp
@@ -1,35 +1,68 @@
-
+service { "mysql":
+  ensure => "stopped",
+}
 
 
 service { "apache2":
   ensure => "stopped",
 }
 
-package { "apache2.2":
-ensure => "purged",
-subscribe => service['apache2'],
-}
+
 
 package { "apache2":
 ensure => "purged",
-subscribe => service['apache2'],
+require => service['apache2'],
 }
 
 
-
-package { "apache2-utils":
-ensure => "purged",
-subscribe => service['apache2'],
-}
-
-
-
-exec { 'autoremove-apache2':
+exec { 'autoremove-apache':
     command => '/usr/bin/apt-get autoremove --purge -y',
     refreshonly => true,
     subscribe => package['apache2'],
 }
 
 
+package { "mysql-server":
+ensure => "purged",
+require => service['mysql'],
+}
+
+package { "mysql-client":
+ensure => "purged",
+require => service['mysql'],
+}
 
 
+package { "mysql":
+ensure => "purged",
+require => service['mysql'],
+}
+
+
+exec { 'autoremove-mysql-server':
+    command => '/usr/bin/apt-get autoremove --purge -y',
+    refreshonly => true,
+    subscribe => package['mysql-server'],
+}
+
+exec { 'autoremove-mysql-client':
+    command => '/usr/bin/apt-get autoremove --purge -y',
+    refreshonly => true,
+    subscribe => package['mysql-client'],
+}
+
+exec { 'autoremove-mysql':
+    command => '/usr/bin/apt-get autoremove --purge -y',
+    refreshonly => true,
+    subscribe => package['mysql'],
+}
+
+
+
+file { "remove_wordpress":
+ensure => absent,
+path => "/var/www",
+recurse => true,
+purge => true,
+force =>true,
+}
