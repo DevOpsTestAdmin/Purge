b5238b469a0a9281db91f299ff192036a0830703
DevOpsTestAdmin <cmanikandan@yahoo.com>
2015-03-11 12:41:15 +0530

    Update uninstall.pp

--- uninstall.pp
+++ uninstall.pp
@@ -1,7 +1,7 @@
 
 service { "apache2":
   ensure => "stopped",
-  before => Package['apache2'],Package['apache2-utils'],Package['apache2.2-bin'],Package['apache2-mpm-prefork'],Package['apache2.2-common'],
+  #before => Package['apache2'],Package['apache2-utils'],Package['apache2.2-bin'],Package['apache2-mpm-prefork'],Package['apache2.2-common'],
 }
 
 
